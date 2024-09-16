8ab8f4800b706b3c28a3bb9dbb84389bca0dfb9a
Sandeep Maraboina <107021866+Sandeep-1405@users.noreply.github.com>
2024-09-16 13:14:41 +0000

    lasyt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import "./styles.css";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.css";
+import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
 
 export default function App() {
   const [userInput, setUserInput] = useState("");
@@ -8,9 +11,10 @@ export default function App() {
 
   useEffect(() => {
     axios
-      .get("www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata")
-      .then((rrr) => {
-        console.log(rrr);
+      .get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + userInput)
+      .then((res) => {
+        console.log(res.data);
+        setDisplayData(res.data.meals);
       })
       .catch((error) => {
         console.log(error);
@@ -27,6 +31,29 @@ export default function App() {
           placeholder="search for yor meal"
           onChange={(e) => setUserInput(e.target.value)}
         />
+      </div>
+      <div>
+        {displayData.length === 0 ? (
+          <h1>No Data Found</h1>
+        ) : (
+          <div>
+            {displayData.map((item) => (
+              <div key={item.idMeal} className="card m-3 p-3">
+                <div className="m-auto">
+                  <img src={item.strMealThumb} className="h-25 w-25" />
+                </div>
+
+                <div>
+                  <h1 className="">{item.strMeal}</h1>
+                  <div className="d-flex justify-content-around m-3">
+                    <h6 className="">Area :{item.strArea}</h6>
+                    <h6 className="">Category :{item.strCategory}</h6>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
